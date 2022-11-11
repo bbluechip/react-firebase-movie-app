67a595de1795cf606b897d06848405f241e219c3
bbluechip <cs.orhan.berk@gmail.com>
2022-11-11 20:57:26 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <AppRouter />
     </div>
   );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
 import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );

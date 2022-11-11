c3fc470aee9cec9115b55dd85134adf364951711
bbluechip <cs.orhan.berk@gmail.com>
2022-11-11 20:48:12 +0100

    install all tailwinds, start creating routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
